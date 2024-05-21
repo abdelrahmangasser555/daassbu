b3a9977d44d29fe1eb496cd74b62a12c04a33b74
yusuf emad <yusufy2004@gmail.com>
2024-05-21 21:17:58 +0300

    added vars in docker

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
 FROM public.ecr.aws/docker/library/python:3.11.0-slim-bullseye
 COPY --from=public.ecr.aws/awsguru/aws-lambda-adapter:0.7.1 /lambda-adapter /opt/extensions/lambda-adapter
 
+ARG VAR1
+ARG VAR2
+
+ENV OPENAI_API_KEY=$VAR1
+ENV PINECONE_API_KEY=$VAR2
+
 # install fastapi and uvicorn
 RUN pip install fastapi uvicorn
 
--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 # simple fast api server
 import uvicorn
 from fastapi import FastAPI
+# import necessary libraries for CORS
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 app = FastAPI()
 
+# add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/docker")
 def read_root():
-    return {"Hello": "World"}
+    openai_api_key = os.environ.get("OPENAI_API_KEY")
+    pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+
+    return {"Hello": "World", "openai_api_key": openai_api_key, "pinecone_api_key": pinecone_api_key}
 
 uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
