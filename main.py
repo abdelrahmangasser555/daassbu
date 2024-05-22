ba4cd32970ce45119d588563b730f276c3e9a4b7
yusuf emad <yusufy2004@gmail.com>
2024-05-22 10:10:18 +0300

    Shorse

--- main.py
+++ main.py
@@ -22,6 +22,6 @@ def read_root():
     openai_api_key = os.environ.get("OPENAI_API_KEY")
     pinecone_api_key = os.environ.get("PINECONE_API_KEY")
 
-    return {"Hello": "shorse", "openai_api_key": openai_api_key, "pinecone_api_key": pinecone_api_key}
+    return {"Hello": "shorse shorse shorse", "openai_api_key": openai_api_key, "pinecone_api_key": pinecone_api_key}
 
 uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
